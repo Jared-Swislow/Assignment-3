b975e0ccb46f6622708651094fce3dddb7b6abd8
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-03-03 13:32:52 -0800

    Update Board.java

--- src/View/Board.java
+++ src/View/Board.java
@@ -1,12 +1,27 @@
 package View;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**Displays the board
  * */
 public class Board extends JPanel {
-	Board() {
-		setBackground(Color.PINK);
-	}
+    JButton button = new JButton();
+    public List<JButton> characters = new ArrayList<JButton>();
+    Board() {
+        setBackground(Color.PINK);
+
+        try {
+            Image img = ImageIO.read(Objects.requireNonNull(getClass().getResource("resources/Alex.bmp")));
+            button.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        this.add(button);
+    }
 }
