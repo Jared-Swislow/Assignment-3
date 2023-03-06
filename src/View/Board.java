5882845059e60ed0d22ac34ba3a314872f376ee2
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-03-06 15:40:55 -0800

    Update Board.java

--- src/View/Board.java
+++ src/View/Board.java
@@ -1,17 +1,49 @@
 package View;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 /**Displays the board
  * */
 public class Board extends JPanel {
-	Board() {
-		setBackground(Color.PINK);
-		GridLayout layout = new GridLayout(3, 8);
-		setLayout(layout);
-		for (int i = 0; i < 24; i++){
-			add(new JButton());
-		}
-	}
+    public List<Person> Characters = new ArrayList<Person>();
+    Board() {
+        setBackground(Color.PINK);
+        getCharacters();
+        if (Characters!=null){
+            for (Person c: Characters){
+                this.add(c);
+            }
+        }
+
+    }
+
+    public void getCharacters(){
+        JButton button = new JButton();
+        System.out.println(new File("Characters.txt").getAbsolutePath());
+        File CharactersFile = new File(Objects.requireNonNull(getClass().getResource("Characters.txt")).getFile());
+        try{
+            Scanner Chars = new Scanner (CharactersFile);
+            while (Chars.hasNextLine()){
+                String data = Chars.nextLine();
+                System.out.println(data);
+                Person Character = new Person(data);
+                Characters.add(Character);
+
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+
+
+
+    }
+
 }
