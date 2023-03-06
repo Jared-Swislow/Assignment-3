6a81d51f883d649af37b543d9e79ee3c1c0ee9a9
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2023-03-06 14:34:41 -0800

    put in dummy layout

--- src/View/Board.java
+++ src/View/Board.java
@@ -8,5 +8,10 @@ import java.awt.*;
 public class Board extends JPanel {
 	Board() {
 		setBackground(Color.PINK);
+		GridLayout layout = new GridLayout(3, 8);
+		setLayout(layout);
+		for (int i = 0; i < 24; i++){
+			add(new JButton());
+		}
 	}
 }
