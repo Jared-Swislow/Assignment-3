6af9ed23815fdcf23af0b9251ef7efb0f479b964
sophie :-) <48779014+smartyro@users.noreply.github.com>
2023-03-08 21:37:07 -0800

    final Message commit

--- src/Model/Message.java
+++ src/Model/Message.java
@@ -1,16 +1,23 @@
 package Model;
 
-/**Stores message data
- * */
+/**
+ *
+ * Stores message data
+ */
 public class Message {
 	String message;
 	boolean fromMe;
-	
+
+	/**
+	 * 
+	 * @param msg
+	 * @param fromMe
+	 */
 	public Message(String msg, boolean fromMe) {
 		this.message = msg;
 		this.fromMe = fromMe;
 	}
-	
+
 	public String toString() {
 		return (fromMe ? "Me" : "Other") + ": " + message;
 	}
