8bca1bcf45ff49612448970d1ec24b500e77fa59
sophie :-) <48779014+smartyro@users.noreply.github.com>
2023-03-08 21:40:54 -0800

    final Person commit

--- src/View/Person.java
+++ src/View/Person.java
@@ -11,7 +11,11 @@ public class Person extends JButton {
     private Image img;
     private String name;
 
-    public Person(String imagePath){
+    /**
+     * Prints out the images to the screen so the player can play the game
+     * @param imagePath
+     */
+    public Person(String imagePath) {
         try {
             img = ImageIO.read(Objects.requireNonNull(getClass().getResource("resources/" + imagePath)));
             img = img.getScaledInstance(100, 150, java.awt.Image.SCALE_SMOOTH);
@@ -24,17 +28,21 @@ public class Person extends JButton {
         this.name = imagePath.split(".bmp")[0];
     }
 
+    /**
+     * Eliminates the person if they are clicked
+     * @return boolean representing if the character has been eliminated
+     */
     public boolean isEliminated() {
         return eliminated;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
     public void setEliminatedTrue() {
         try {
-            img = ImageIO.read(Objects.requireNonNull(getClass().getResource("resources/yellow.jpg")));
+            img = ImageIO.read(Objects.requireNonNull(getClass().getResource("resources/yellow.bmp")));
             img = img.getScaledInstance(100, 150, java.awt.Image.SCALE_SMOOTH);
             setIcon(new ImageIcon(img));
         } catch (Exception ex) {
@@ -43,6 +51,11 @@ public class Person extends JButton {
         this.eliminated = true;
     }
 
+    /**
+     * 
+     * Sets the 'correct' boolean if the user chose correctly
+     * @param correct
+     */
     public void setCorrect(boolean correct) {
         this.correct = correct;
     }
