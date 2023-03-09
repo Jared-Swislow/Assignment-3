7cdae7e52bc130eb67f7b4469df62bcb1c0472cd
sophie :-) <48779014+smartyro@users.noreply.github.com>
2023-03-08 21:39:44 -0800

    final Chat commit

--- src/View/Chat.java
+++ src/View/Chat.java
@@ -3,69 +3,66 @@ package View;
 import Controller.KeyHandler;
 import Model.ChatLog;
 import Model.Message;
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
-/**Displays chat
- * */
+/**
+ * 
+ * Displays chat
+ */
 public class Chat extends JPanel implements Observer {
 	private static final String noChatYet = "No Chat yet...";
-	
+
 	private final JTextArea displayArea;
 	private final JTextArea writeArea;
+
 	Chat() {
 		BorderLayout borderLayout = new BorderLayout();
 		setLayout(borderLayout);
-		
+
 		setBackground(Color.GRAY);
-		
+
 		displayArea = new JTextArea(noChatYet);
 		displayArea.setEditable(false);
 		displayArea.setRows(5);
 		displayArea.setLineWrap(true);
 
 		add(displayArea, BorderLayout.NORTH);
-		
+
 		writeArea = new JTextArea();
 		writeArea.setBackground(Color.lightGray);
 		writeArea.setRows(4);
 		writeArea.addKeyListener(KeyHandler.getInstance());
 		writeArea.setLineWrap(true);
 		KeyHandler.getInstance().setWriteArea(writeArea);
-		
-		add(writeArea, BorderLayout.SOUTH);
 
+		add(writeArea, BorderLayout.SOUTH);
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-		
-		// TODO: 3/1/2023  
 	}
-	
-	//Updates the displayed messages by adding the last one
+
+	// Updates the displayed messages by adding the last one
 	@Override
 	public void update(Observable o, Object arg) {
-		//Empty the display area if this is the first message
+		// Empty the display area if this is the first message
 		String message = displayArea.getText();
-		if(message.equals(noChatYet)) {
+		if (message.equals(noChatYet)) {
 			displayArea.setText("");
 		}
 		ArrayList<Message> m = ChatLog.getInstance().getMessages();
 		displayArea.append(m.get(m.size() - 1).toString());
-
-		//repaint();
 	}
-	
+
 	JTextArea getDisplayArea() {
 		return displayArea;
 	}
-	
+
 	JTextArea getWriteArea() {
 		return writeArea;
 	}
